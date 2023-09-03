5f024b6f3f7c2add17a0cd32a7075c379e172eb7
Len Lanphar <lanphar@gmail.com>
2023-09-03 19:42:38 -0400

    added LLM plot generation

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 import tracery_plot
 from plot_image import PlotImage
-
+import llm_plot
 import gradio as gr
 
 def generate_tracery():
     return tracery.generate()
 
 def generate_llm(tracery_output):
-    return "BOT OUTPUT: "+tracery_output 
+    return llm.generate(tracery_output)
 
 def generate_image(llm_output):
     return plot_image.render(llm_output) 
@@ -20,13 +20,16 @@ with gr.Blocks() as server:
     llm_output = gr.Textbox(label="LLM Output",interactive=True)
     generate_llm_btn = gr.Button("Generate LLM Plot")
     generate_llm_btn.click(fn=generate_llm, inputs=tracery_output, outputs=llm_output, api_name="generate_llm")
-    image_output = gr.Image(label="Image",height=500)
+    image_output = gr.Image(label="Image",height=300)
     generate_image_btn = gr.Button("Generate Image")
     generate_image_btn.click(fn=generate_image, inputs=llm_output, outputs=image_output, api_name="generate_image")
                            
 
+MODEL_FILE = "/home/llanphar/src/oobabooga_linux/text-generation-webui/models/Wizard-Vicuna-30B-Uncensored.ggmlv3.q4_K_M.bin"
+
 tracery = tracery_plot.TraceryPlot("mallhark.json")
 plot_image = PlotImage() 
+llm = llm_plot.LLMPlot(MODEL_FILE,n_gpu_layers=30)
 
 server.launch(server_name="0.0.0.0")
 
