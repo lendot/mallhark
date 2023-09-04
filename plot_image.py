c3ebb40606ff9e10f2ceeeea85be7bdbcf8d694d
Len Lanphar <lanphar@gmail.com>
2023-09-04 17:48:00 -0400

    fixed font color

--- plot_image.py
+++ plot_image.py
@@ -33,7 +33,7 @@ class PlotImage:
         with Image(filename = template_image) as canvas:
 
             with Drawing() as context:
-                font = Font(FONT_FILE, color = Color('#a00000'))
+                font = Font(FONT_FILE, color = Color('#650000'))
                 context(canvas)
                 canvas.caption(
                         plot, 
